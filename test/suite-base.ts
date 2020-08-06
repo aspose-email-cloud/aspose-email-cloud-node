2719d6e0705ea0c8f283168807a7d67e293c6f60
Marat Gumerov <marat.gumerof@aspose.com>
2020-08-06 20:12:24 +0400

    Test migration in progress

--- test/suite-base.ts
+++ test/suite-base.ts
@@ -1,15 +1,14 @@
-import {EmailApi} from "../src/api";
+import {EmailCloud} from "../src/api";
 import uuidv4 from "uuid/v4";
-import * as requests from "../src/model/requests/requests";
 import 'mocha';
-import * as models from "../src/model/model";
 import {Suite} from "mocha";
+import {CreateFolderRequest, DeleteFolderRequest, StorageFolderLocation} from "../src/model";
 
 class SuiteBase {
-    public api: () => EmailApi;
+    public api: () => EmailCloud;
     public folder: () => string;
     public storage: () => string;
-    constructor(getApi: () => EmailApi, getFolder: () => string, getStorage: () => string) {
+    constructor(getApi: () => EmailCloud, getFolder: () => string, getStorage: () => string) {
         this.api = getApi;
         this.folder = getFolder;
         this.storage = getStorage;
@@ -24,55 +23,25 @@ class SuiteBase {
         return result;
     }
 
-    public async createCalendar(startDate?: Date): Promise<string> {
-        const fileName = uuidv4() + '.ics';
-        startDate = (startDate == null) ? this.getDate(undefined, 1) : startDate;
-        const endDate = this.getDate(startDate, 1);
-        await this.api().createCalendar(new requests.CreateCalendarRequest(
-            fileName,
-            new models.HierarchicalObjectRequest(
-                new models.HierarchicalObject(
-                    'CALENDAR',
-                    undefined, [
-                        new models.PrimitiveObject("LOCATION", undefined, "location"),
-                        new models.PrimitiveObject("STARTDATE", undefined, startDate.toUTCString()),
-                        new models.PrimitiveObject("ENDDATE", undefined, endDate.toUTCString()),
-                        new models.HierarchicalObject("ORGANIZER", undefined, [
-                            new models.PrimitiveObject("ADDRESS", undefined, "organizer@am.ru"),
-                            new models.PrimitiveObject("DISPLAYNAME", undefined, "Organizer Name")
-                        ]),
-                        new models.HierarchicalObject("ATTENDEES", undefined, [
-                            new models.IndexedHierarchicalObject("ATTENDEE", undefined, 0, [
-                                new models.PrimitiveObject("ADDRESS", undefined, "attendee@am.ru"),
-                                new models.PrimitiveObject("DISPLAYNAME", undefined, "Attendee Name")
-                            ])
-                        ])]
-                ),
-                new models.StorageFolderLocation(this.storage(), this.folder())
-            )
-        ));
-        return fileName;
-    }
-
-    public getStorageFolderLocation(): models.StorageFolderLocation {
-        return new models.StorageFolderLocation(this.storage(), this.folder());
+    public getStorageFolderLocation(): StorageFolderLocation {
+        return new StorageFolderLocation(this.storage(), this.folder());
     }
 }
 
 export function suiteBase(suite: Suite): SuiteBase {
-    let api: EmailApi;
+    let api: EmailCloud;
     let folder: string;
     const storage = 'First Storage';
     suite.timeout(100000);
     suite.beforeAll(async () => {
-        api = new EmailApi(process.env.appSid, process.env.appKey, process.env.apiBaseUrl);
+        api = new EmailCloud(process.env.appSid, process.env.appKey, process.env.apiBaseUrl);
         const authUrl = process.env.authUrl;
         if (authUrl != null) api.configuration.authUrl = authUrl;
         folder = uuidv4();
-        await api.createFolder(new requests.CreateFolderRequest(folder, storage));
+        await api.cloudStorage.folder.createFolder(new CreateFolderRequest(folder, storage));
     });
     suite.afterAll(async () => {
-        await api.deleteFolder(new requests.DeleteFolderRequest(folder, storage, true));
+        await api.cloudStorage.folder.deleteFolder(new DeleteFolderRequest(folder, storage, true));
     });
 
     return new SuiteBase(() => api, () => folder, () => storage);
